83cbccd9f513332511c535307ddb2ce1b2970af2
tttttyj <554748189@qq.com>
2019-06-27 20:45:39 +0800

    超市购物 0627修改

--- tyj/0626/0626homework2.py
+++ tyj/0626/0626homework2.py
@@ -1,53 +1,18 @@
 # encoding:utf-8
-# 1.数据结构
 # 定义会员信息
 members=[
-    {'id':'1','tel':'131','disc':'0.98'},
-    {'id':'2','tel':'132','disc':'0.9'},
-    {'id':'3','tel':'133','disc':'0.8'}
+    {'id':'1','tel':'131','disc':0.98},
+    {'id':'2','tel':'132','disc':0.9},
+    {'id':'3','tel':'133','disc':0.8}
 ]
-# 2.算法单元
-#  定义一个获取会员信息的方法
-
-#  获取用户购买的商品信息
-# product_num1=0
-# def get_product_input():
-#     prod_info = raw_input("请输入商品价格，或输入Q进行结算")
-#     global product_num1
-#     product_num1 = product_num1+1
-#     if product_num1 ==3:
-#         return "Q"
-#     return 100
-# #  依据输入的商品信息，计算商品的结算清单以及总价
-# def goods_money(goods_list):
-#     goods_list=[
-#         [1,100,95],
-#         [2,200,190],
-#         [3,300,285]
-#     ]
-#     goods_total= 570
-#     return goods_list, goods_total
-# #  格式化输出内容
-
 # 3.主程序
 # 1.获取用户手机号，并通过手机号判断是否会员，返回会员折扣
 def member_disc(user_tel):
     for member in members:
-        if member['tel'] == user_tel:
+        if user_tel == member['tel']:
+            print member['tel']
             return member['disc']
     return 1.0
-while True:
-    try:
-        user_tel = int(input('请输入手机号'))
-        user_disc = member_disc(user_tel)
-        break
-    except:
-        print ("输入有误，请输入正确的手机号")
-
-
-
-# 2.录入商品信息
-goods_list = []
 def get_product_input():
     while True:
         try:
@@ -57,6 +22,27 @@ def get_product_input():
             if user_input == "Q":
                 return "Q"
             print ("输入有误，请重新输入")
+def payment(goods_list,user_disc):
+    payment_list=[]
+    total = 0
+    for i in range(len(goods_list)):
+        payment1= [i+1,goods_list[i],goods_list[i]*user_disc]
+        payment_list.append(payment1)
+        total += goods_list[i]*user_disc
+    return payment_list,total
+def format_out_msg(list,total):
+    out_msg = "商品ID\t原价\t折后价\n"
+    for prod in list:
+        out_msg += "%s\t%s\t%s\n" % (prod[0], prod[1], prod[2])
+    out_msg+= "--------------------\n"
+    out_msg+= "总价=\t%s" %total
+    return out_msg
+
+
+user_tel = raw_input('请输入手机号')
+user_disc = member_disc(user_tel)
+# 2.录入商品信息
+goods_list = []
 while True:
         prod_info = get_product_input()
         print ('商品价格为：%s'% prod_info)
@@ -66,29 +52,9 @@ while True:
             print ('商品已添加至商品清单')
             goods_list.append(prod_info)
 print ("商品清单 %s" %goods_list)
-
 # 3.计算商品的结算清单
-def payment(goods_list,user_disc):
-    payment_list=[]
-    total = 0
-    for i in range(len(goods_list)):
-        payment1= [i+1 , goods_list(i) , goods_list(i)*user_disc]
-        payment_list.append(payment1)
-        total = total + goods_list(i)*user_disc
-    return goods_list, total
 
-
+pay_list,pay_total = payment(goods_list,user_disc)
 # 4.格式化输出内容
-def format_out_msg(list,total):
-    out_msg = "商品ID\t原价\t折后价"
-    for prod in list:
-        out_msg= out_msg + "%s\t,%s\t,%s\n" %(prod[0],prod[1],prod[3])
-    out_msg+= "--------------------\n"
-    out_msg+= "总价=\t%s" %total
-    return out_msg
-
-out_put = format_out_msg()
-
-
-
-    return out_msg
+out_put = format_out_msg(pay_list, pay_total )
+print out_put
