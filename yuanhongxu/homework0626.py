f7b52d3557bd1a859cb2334d6c270e7bc5b48e0b
hongxuY <1944783476@qq.com>
2019-06-27 19:21:00 +0800

    1.猜数字作业更新 2.超市收银作业

--- yuanhongxu/homework0626.py
+++ yuanhongxu/homework0626.py
@@ -6,22 +6,25 @@ random_number=random.randint(0,100)
 print (random_number)
 count=0
 while True:
-    count += 1
-    while True:
-        try:
-            number=int(input("请输入你猜的数值"))
+    try:
+        count += 1
+        number=input("请输入你猜的数值")
+        if isinstance(number,float):
+            print ("不能输入小数")
+        elif number==count:
+            print("输入错误，请重新输入")
+        elif number==random_number:
+            print ("恭喜你猜对了，猜了%d次"%(count))
             break
-        except:
-            print ("输入错误，请重新输入")
+        elif number>random_number:
+            print ("猜大了")
+        else:
+            print ("猜小了")
+    except:
+        print ("输入错误，请重新输入")
 
 
-    if number==random_number:
-        print ("恭喜你猜对了，猜了%d次"%(count))
-        break
-    elif number>random_number:
-        print ("猜大了")
-    else:
-        print ("猜小了")
+
 
 
 
