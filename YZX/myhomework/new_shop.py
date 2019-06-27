8581b2593958fe055fa00b51d7b0dba13f58a142
zhenxingyan <38336539+pingshaluoyan@users.noreply.github.com>
2019-06-27 21:11:23 +0800

    手打作业完成+classmethod

--- YZX/myhomework/new_shop.py
+++ YZX/myhomework/new_shop.py
@@ -1,17 +1,18 @@
 #encoding:utf-8
-members=[
-    {'id':'1','tel':'18845871680','disc':0.9},
-    {'id':'2','tel':'18845095099','disc':0.1}
-]
+from members import Members
+# members=[
+#     {'id':'1','tel':'18845871680','disc':0.9},
+#     {'id':'2','tel':'18845095099','disc':0.1}
+# ]
 shop_list=[]
 pay_list=[]
 #根据手机号码获取会员折扣
-def get_disc_by_tell(tel):
-    for member in members:
-        if tel==member['tel']:
-            print member['disc']
-            return member['disc']
-    return 1.0
+# def get_disc_by_tell(tel):
+#     for member in members:
+#         if tel==member['tel']:
+#             print member['disc']
+#             return member['disc']
+#     return 1.0
 #商品价格
 def input_price():
     while True:
@@ -41,7 +42,7 @@ def out_way(new_pay_list):
     return  msg
 
 tel=raw_input("输入你的手机号码：")
-disc=get_disc_by_tell(tel)
+disc=Members.get_disc_by_tell(tel)
 while True:
 #输入商品，Q退出输入
     price=input_price();
