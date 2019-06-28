cdbb13e8dcd58e7db253f11c65f660486f693c24
hongxuY <1944783476@qq.com>
2019-06-28 15:02:21 +0800

    更新

--- yuanhongxu/shopping/shopping.py
+++ yuanhongxu/shopping/shopping.py
@@ -2,18 +2,24 @@
 from huiyuan import huiyuan_help
 from sangping import sangping_caozuo
 
-user_tel=raw_input("请输入你的手机号")
-disc=huiyuan_help.tel_disc(user_tel)
+def saling():
+    user_tel=raw_input("请输入你的手机号")
+    disc=huiyuan_help.tel_disc(user_tel)
 
-spjg_list = []
-while True:
-    shangpin=sangping_caozuo.sp_spjg()
-    if shangpin=="Q":
-        break
-    else:
-        spjg_list.append(shangpin)
+    spjg_list = []
+    while True:
+        shangpin=sangping_caozuo.sp_spjg()
+        if shangpin=="Q":
+            break
+        elif shangpin==None:
+            continue
+        else:
+            spjg_list.append(shangpin)
 
-price_list,sum_price=sangping_caozuo.jisuan(spjg_list,disc)
+    price_list,sum_price=sangping_caozuo.jisuan(spjg_list,disc)
 
-out_put=sangping_caozuo.format(price_list,sum_price)
-print (out_put)
+    out_put=sangping_caozuo.format(price_list,sum_price)
+    print (out_put)
+
+if __name__=="__main__":
+    saling()
