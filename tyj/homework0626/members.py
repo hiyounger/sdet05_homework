28a0a844286e1438a30024073e6f3293ffce7989
tttttyj <554748189@qq.com>
2019-07-01 00:53:29 +0800

    0630作业——会员增删改查

--- tyj/homework0626/members.py
+++ tyj/homework0626/members.py
@@ -38,8 +38,6 @@ class membershelp():
     def get_mermber_by_tell_last_four(cls,tel_last_four):
         for member in members:
             tell_last_four = float(member['tel'])%10000
-            i = float(member['tel'])
-            a = i % 10000
             if tell_last_four == a:
                 print ('会员编号:%s \t 电话:%s \t 折扣:%s'%(member['id'],member['tel'],member['disc']))
                 return 1
